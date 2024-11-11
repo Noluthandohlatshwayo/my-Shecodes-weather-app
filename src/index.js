28d82b5688a8fe4650cebbbdd486c0e767fad212
noluthandohlatshwayo <noluthandohlatshwayo9@gmail.com>
2024-11-11 14:50:52 +0200

    updated html,css and js

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+  getForecast(repsonse.data.city);
 }
 
 function formatDate(date) {
@@ -52,7 +54,39 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function getForecast(city) {
+  let apiKey = "d8312dao2c0bce4b879864fd3ac6af5t";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+<div class="weather-forecast-day">
+  <div class="weather-forecast-date">${day}</div>
+    <div class="weather-forecast-icon">🌥️</div>
+      <div class="weather-forecast-temperatures">
+        <div class="weather-forecast-temperature"><strong>15°</strong></div>
+         <div class="weather-forecast-temperature">9°</div> 
+   </div>
+   </div>
+`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Johannesburg");
+getForecast();
